24e3388520799c77de0377a3322bc7d0392c3fc9
Reema Hanim Hanass <220374295+binary69@users.noreply.github.com>
2026-01-03 13:19:44 +0530

    Time: 1 ms (99.81%), Space: 43.4 MB (20.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,11 +1,9 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character,Integer> vals = new HashMap<>();
-        for(char c: s.toCharArray()) vals.put(c, vals.getOrDefault(c,0)+1);
-        for(char c: t.toCharArray()){
-            if(vals.getOrDefault(c,0)==0) return c;
-            vals.put(c,vals.get(c)-1);
-        }
-        return '\0';
+       int XOR = 0;
+       for(char c: s.toCharArray()) XOR ^=c;
+       for(char c: t.toCharArray()) XOR ^=c;
+
+       return (char)XOR;
     }
 }
